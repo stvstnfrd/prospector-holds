1e8da305263913a4cf123db9d630fa47a48bef58
stvstnfrd <stvstnfrd@noreply.users.github.com>
2024-08-09 23:19:27 -0600

    test: add basic test suite for autodiscovery

---
+++ src/prospector_holds/tests/test_basic.py
@@ -0,0 +1,13 @@
+"""
+Perform basic tests
+"""
+import unittest
+
+
+class TestBasic(unittest.TestCase):
+    def test_basic(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
