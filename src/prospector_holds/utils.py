b57be16c677697dd65bafac99fc7e14d1502028c
stvstnfrd <stvstnfrd@noreply.users.github.com>
2024-08-14 13:14:00 -0600

    fix: remove backslash from label/key names

--- src/prospector_holds/utils.py
+++ src/prospector_holds/utils.py
@@ -7,10 +7,11 @@ def label_to_key(label):
     Convert a label (of space-delimited words) into a dict key
 
     Assume: label matches the following regex:
-        ^[a-zA-Z][-_ a-zA-Z0-9]*$
+        ^[a-zA-Z][-_/ a-zA-Z0-9]*$
     """
     key = label
     key = ' '.join(key.split('-'))
+    key = ' '.join(key.split('/'))
     key = '_'.join(key.split(' '))
     key = key.lower()
     return key
